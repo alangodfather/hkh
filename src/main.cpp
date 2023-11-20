e00cce3e6a693827e26b616e8bcdae046b60c22a
alangodfather <Liowen919@gmail.com>
2023-11-19 21:45:00 -0800

    after aomp

--- src/main.cpp
+++ src/main.cpp
@@ -68,33 +68,24 @@ void competition_initialize() {}
  * from where it left off.
  */
 void autonomous() {
-	intake = -127;   // first triball push
-	PIDdrive(68,0.25,0.01,5,1350);
-	PIDturn(-52,1.9,0.00025,5);
-	PIDdrive(-50,0.25,0.01,2.5,800);
-	PIDdrive(8,0.25,0.01,2.5,800);
-	intake = 0; 
-	PIDturn(-179,2.2,0.000275,7.5);
-	intake = 100;
-	delay(200);
-	PIDdrive(11,0.25,0.01,2.5,800);
-	intake = 0;
-	PIDdrive(-20,0.20,0.01,2.5,800);
-	PIDturn(129,2.2,0.000275,7.5);
 	intake = -127;
-	delay(250);
-	PIDdrive(33,0.10,0.01,2.5,900);
-	PIDdrive(-13,0.25,0.01,2.5,800);
-	PIDturn(-125,1.9,0.0003,5);
-	intake = 70;
-	PIDdrive(25,0.25,0.01,2.5,800);
-	PIDdrive(-10,0.20,0.01,2.5,800);
+	PIDdrive(46,0.22,0.01,5,1350);
+	delay(500);
+	PIDdrive(-47,0.25,0.01,5,1350);
+	PIDturn(75,1.9,0.00025,5);
+	PIDdriverightArc(-45,0.4,0.01,0.2,2200);
 	intake = 0;
-	PIDturn(-90,1.9,0.00025,5);
-	PIDdrive(-37,0.25,0.01,2.5,800);
-	PIDturn(-90,1.9,0.00025,5);
-	PIDdriveleftArc(-77,0.33,0.0025,1,1100);// last triball push
-	PIDdrive(5,0.25,0.01,2.5,800);
+	PIDdrive(4,0.25,0.01,5,1350);
+	delay(300);
+	PIDdrive(-6,0.25,0.01,5,1350);
+	delay(300);
+	PIDdrive(4,0.25,0.01,5,1350);
+	
+	
+	
+	
+	
+	
 
 	
 	//new close side legal elim auto
@@ -239,7 +230,7 @@ void autonomous() {
 
 	// Working Skills auto code (Slot 3)
 	/*
-	timedcata(4000,-127);
+	timedcata(30000,-127);
 	PIDturn(-27,2,0.0025,2);
 	resetcata();
 	PIDdrive(-79,0.25,0.01,0.2,5000);
